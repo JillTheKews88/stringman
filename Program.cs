c74fc46c1d607632049efb617b9ee2a8d8c4b255
JillTheKews88 <roaticata14@gmail.com>
2023-08-23 10:57:41 +0200

    23aug

--- Program.cs
+++ Program.cs
@@ -1,33 +1,50 @@
-﻿namespace stringman;    
+﻿using System.ComponentModel;
+
+namespace stringman;    
 class Program
 {
     static void Main(string[] args)
     {
         using (var reader = new StreamReader("flyvning1.csv"))
         {
+            int counter = 0;
             while (!reader.EndOfStream)
             {
-                //Console.WriteLine(reader.ReadLine());
+                //Console.WriteLine(reader.ReadLine())
                 var line = reader.ReadLine();
-                string[] values = line.Split(';');
-                int i = values.Length;
-                for (int j = 0; j < i; j++)
+                List<string> values = new List<string>();
+                values = line.Split(';').ToList();
+                int i = values.Count;
+                if (counter == 0)
                 {
-                    if (j == 2 || j == 3)
+                    values.Add("GPSKoord");
+                    counter++;
+                }
+                else
+                {
+                    for (int j = 0; j < i; j++)
                     {
-                    
-                        string newJ = values[j].Replace(".", string.Empty);
-                        if (j == 2)
+                        if (j == 2 || j == 3)
                         {
-                            newJ = newJ.Insert(1, ".");
+
+                            string newJ = values[j].Replace(".", string.Empty);
+                            if (j == 2)
+                            {
+                                newJ = newJ.Insert(1, ".");
+                                values[j] = newJ;
+                            }
+                            if (j == 3)
+                            {
+                                newJ = newJ.Insert(2, ".");
+                                values[j] = newJ;
+                            }
                         }
-                        if (j == 3)
-                        {
-                            newJ = newJ.Insert(2, ".");
-                        }
-                        Console.WriteLine(newJ);
+                        Console.WriteLine(values[j]);
                     }
-                    //Console.WriteLine(values[j]);
+                    Console.WriteLine("Counter: " + counter);
+                    values.Add(values[2] + "," + values[3]);
+                    counter++;
+
                 }
             }
         }
